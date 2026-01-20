ddff713fe1c5824a3a114cd718e1671fb23b037c
MKay <130282288+MOHIT-KUMAR0001@users.noreply.github.com>
2026-01-20 14:31:34 +0000

    fixing agent folder structure and decrease latency in sending streams

--- agent/src/__init__.py
+++
@@ -1,8 +0,0 @@
-from .model import create_event
-from .logger import Logging
-from .config import config
-from .agent import start
-
-log = Logging()
-
-__all__ = ["start", "create_event", "log", "config"]
